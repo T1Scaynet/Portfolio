88cb554e78272c7558989e06c8ca4885f8fabe1c
Gustavo Chura <gustavochura94@gmail.com>
2023-04-19 03:08:48 -0300

    Portfolio V1.0.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
